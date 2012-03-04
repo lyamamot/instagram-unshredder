2f231fd81125429b38b192ae962de244489b3d84
Leo Yamamoto <leo.yamamoto@gmail.com>
2012-03-03 17:47:40 -0800

    Base files for unshredder challenge.

---
+++ shredder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from PIL import Image
+from random import shuffle
+
+SHREDS = 10
+image = Image.open("station.jpg")
+shredded = Image.new("RGBA", image.size)
+width, height = image.size
+shred_width = width/SHREDS
+sequence = range(0, SHREDS)
+shuffle(sequence)
+
+for i, shred_index in enumerate(sequence):
+    shred_x1, shred_y1 = shred_width * shred_index, 0
+    shred_x2, shred_y2 = shred_x1 + shred_width, height
+    region =image.crop((shred_x1, shred_y1, shred_x2, shred_y2))
+    shredded.paste(region, (shred_width * i, 0))
+
+shredded.save("station_shredded.png", "PNG")
